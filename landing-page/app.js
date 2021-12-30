b7f6bb3885c9026db0c8369fd93ed7b9b0058bcc
choisung2 <c0817d@gmail.com>
2021-12-30 18:57:45 +0900

    add menu-area remove when scrolling

--- landing-page/app.js
+++ landing-page/app.js
@@ -1,21 +1,30 @@
-const btn = document.querySelector('.hamburger')
-const menu = document.querySelector('.dim')
+//menuBtn toggle
+
+const btn = document.querySelector('.nav-trigger')
+const dimMenu = document.querySelector('.dim')
 const menuWrap = document.querySelector('.gnb-menu')
 const closeBtn = document.querySelector('.arrow')
 
 btn.addEventListener('click', () => {
-  menu.classList.toggle("menu-show")
+  dimMenu.classList.toggle("menu-show")
 })
 
-menu.addEventListener('click', (e) => {
+dimMenu.addEventListener('click', (e) => {
   if(e.target !== btn && !menuWrap.contains(e.target)) {
-    menu.classList.remove("menu-show")
+    dimMenu.classList.remove("menu-show")
   }
 })
 
 closeBtn.addEventListener('click', () => {
-    menu.classList.remove("menu-show")
+    dimMenu.classList.remove("menu-show")
 })
+
+window.addEventListener('scroll', function () {
+    dimMenu.classList.remove("menu-show")
+})
+
+
+//topBtn
 
 const goTopBtn = document.querySelector('.go-top')
 
@@ -24,24 +33,29 @@ goTopBtn.addEventListener('click', function() {
 })
 
 window.addEventListener('scroll', () => {
-  if(window.pageYOffset > 110) {
+  if (110 < window.pageYOffset ) {
       goTopBtn.classList.add("go-top-show")
     } else {
       goTopBtn.classList.remove("go-top-show")
     }
 
   if(matchMedia("screen and (max-width: 835px)").matches) {
-    if(window.pageYOffset > 493) {
+    if(493 < window.pageYOffset) {
       goTopBtn.classList.add("go-top-show")
     } else {
+      goTopBtn.classList.remove("go-top-show")
+    }
+  }
+
+  if(matchMedia("screen and (max-width: 475px)").matches) {
+    if(window.pageYOffset > 2546) {
       goTopBtn.classList.remove("go-top-show")
     }
   }
 })
 
 
-
-
+// modal
 
   const modalBtn = document.querySelector(".btn-modal")
   const modal = document.querySelector(".modal")
@@ -58,12 +72,14 @@ window.addEventListener('scroll', () => {
     modal.classList.toggle("show-modal")
     dim.classList.toggle("show-modal")
     bodyScrolling.classList.toggle("stop-scrolling")
+    goTopBtn.classList.remove("go-top-show")
   })
 
   okBtn.addEventListener('click', () => {
     modal.classList.remove("show-modal")
     dim.classList.remove("show-modal")
     bodyScrolling.classList.remove("stop-scrolling")
+    goTopBtn.classList.add("go-top-show")
   })
 
   dim.addEventListener('click', (e) => {
@@ -71,5 +87,6 @@ window.addEventListener('scroll', () => {
       modal.classList.remove("show-modal")
       dim.classList.remove("show-modal")
       bodyScrolling.classList.remove("stop-scrolling")
+      goTopBtn.classList.add("go-top-show")
     }
   })
